a05ce80162f6f20394b1e3b012d2693373d012c5
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-06-05 11:04:13 +0300

    Add files via upload

---
+++ Accauntant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46_5
+{
+    internal class Accauntant
+    {
+        public enum Post
+        {
+            User, Manager, Tester, Director
+        }
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if (hours >= 200)
+            {
+                Console.WriteLine($"Работник {worker} получает премию.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Работник {worker} не получает премию.");
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+            Accauntant.Post i;
+            bool flag = true;
+            int hours = 0;
+            while (flag)
+            {
+                Console.WriteLine($"Выберите роль работника ({Accauntant.Post.User}, {Accauntant.Post.Manager}, {Accauntant.Post.Tester}, {Accauntant.Post.Director}):");
+                string s = Console.ReadLine();
+                switch (s)
+                {
+                    case "User":
+                        Console.Clear();
+                        i = Accauntant.Post.User;
+                        Console.Write("Введите количество отработанных часов: ");
+                        hours = int.Parse(Console.ReadLine());
+                        accauntant.AskForBonus(i, hours);
+                        Console.WriteLine();
+                        break;
+                    case "Manager":
+                        Console.Clear();
+                        i = Accauntant.Post.Manager;
+                        Console.Write("Введите количество отработанных часов: ");
+                        hours = int.Parse(Console.ReadLine());
+                        accauntant.AskForBonus(i, hours);
+                        Console.WriteLine();
+                        break;
+                    case "Tester":
+                        Console.Clear();
+                        i = Accauntant.Post.Tester;
+                        Console.Write("Введите количество отработанных часов: ");
+                        hours = int.Parse(Console.ReadLine());
+                        accauntant.AskForBonus(i, hours);
+                        Console.WriteLine();
+                        break;
+                    case "Director":
+                        Console.Clear();
+                        i = Accauntant.Post.Director;
+                        Console.Write("Введите количество отработанных часов: ");
+                        hours = int.Parse(Console.ReadLine());
+                        accauntant.AskForBonus(i, hours);
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неверный выбор, попробуйте еще раз.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
