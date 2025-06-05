83e69e2c04eeb44a5d3e90960a211be29304dd19
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-06-05 11:02:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    enum Months
+    {
+        январь, февраль, март, апрель, май, июнь, июль, август, сентябрь, октябрь, ноябрь, декабрь
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Months i;
+            for (i = Months.январь; i<= Months.декабрь; i++)
+            {
+                Console.WriteLine($"{(int)i} {i}");
+            }
+            Console.Read();
+        }
+    }
+}
